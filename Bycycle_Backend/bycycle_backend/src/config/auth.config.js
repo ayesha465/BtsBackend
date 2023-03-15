9281f2e0e01da457ff11413185b8f6fcb01fee36
AyeshaRashid <113904276+ayesha465@users.noreply.github.com>
2023-03-15 18:31:39 +1100

    Delete Bycycle_Backend/bycycle_backend directory

--- Bycycle_Backend/bycycle_backend/src/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "secret": "some-secret-shit-goes-here",
-  "refreshTokenSecret": "some-secret-refresh-token-shit",
-  
-  "jwtExpiration": 3600,          
-  "jwtRefreshExpiration": 86400,  
-}
